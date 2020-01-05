44494cfcbc2b059c924b9f6c8ace75265918de27
tr4v3nz <tr4v3nztantra@gmail.com>
2020-01-05 12:40:51 +0700

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.SortNamefromFile(@"c:\names.txt");
+        }
+
+        public void SortNamefromFile(string urlFile)
+        {
+            // Read File from the Url
+            string[] nameList = TxtReader.ReadFile(urlFile);
+            // Declare array for sorting
+            List<string> familyNameList = new List<string>();
+            List<string> sortedNameList = new List<string>();
+            // Sort nameList
+            nameList = nameList.OrderBy(x => x).ToArray();
+            // Get Family Name for each name
+            foreach (string name in nameList)
+            {
+                familyNameList.Add(GetFamilyName(name));
+            }
+            // Sort Family Name
+            familyNameList.Sort();
+            // Sort Name based on sorted Family Name
+            foreach (string familyName in familyNameList)
+            {
+                foreach (string name in nameList)
+                {
+                    if (GetFamilyName(name) == familyName)
+                    {
+                        if (!sortedNameList.Contains(name))
+                        {
+                            sortedNameList.Add(name);
+                        }
+                    }
+                }
+            }
+            // Write each name into console
+            foreach (string sortedName in sortedNameList)
+            {
+                Console.WriteLine(sortedName);
+            }
+            TxtReader.WriteFile(sortedNameList);
+        }
+
+        public string GetFamilyName(string fullName)
+        {
+            string[] splitName = fullName.Split(' ');
+            return splitName[splitName.Count() - 1];
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/TxtReader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class TxtReader
+    {
+        public static string[] ReadFile(string fileUrl)
+        {
+            string[] names = File.ReadAllLines(fileUrl, Encoding.UTF8);
+            return names;
+        }
+
+        public static void WriteFile(List<string> data)
+        {
+            string[] lines = new string[data.Count];
+            int index = 0;
+            string fileName = @"C:\Users\tr4v3\Documents\sorted-names-list.txt";
+            // Map the names to array of string
+            foreach (string item in data)
+            {
+                lines[index] = item;
+                index++;
+            }
+            // Check if file already exists. If yes, delete it.     
+            if (File.Exists(fileName))
+            {
+                File.Delete(fileName);
+            }
+
+            // Write the array of string to the new file
+            System.IO.File.WriteAllLines(fileName, lines);
+        }
+    }
+}
